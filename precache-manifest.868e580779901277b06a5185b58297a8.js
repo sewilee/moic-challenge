588517b7080912ec7e81672aa21b186e62e8a21e
sewilee <sewilee@gmail.com>
2019-10-18 11:58:37 -0400

    Updates

---
+++ precache-manifest.868e580779901277b06a5185b58297a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023b8307a5a8c4bfc61a3d7f0bf4bf62",
+    "url": "/moic-challenge/index.html"
+  },
+  {
+    "revision": "53d476181dafd99850d5",
+    "url": "/moic-challenge/static/css/main.d9a4a06a.chunk.css"
+  },
+  {
+    "revision": "6d4fd88f71ade11d5ff4",
+    "url": "/moic-challenge/static/js/2.f3c5566a.chunk.js"
+  },
+  {
+    "revision": "53d476181dafd99850d5",
+    "url": "/moic-challenge/static/js/main.27f43c76.chunk.js"
+  },
+  {
+    "revision": "9a019de09156c60285cc",
+    "url": "/moic-challenge/static/js/runtime-main.12c33d0e.js"
+  }
+]);
